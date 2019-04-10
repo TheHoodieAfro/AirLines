dcb45e0af4743d8f0be12828643c44cf32cc3466
Cristian Sanchez Pelaez <crisp937@hotmail.com>
2019-04-09 23:07:35 -0500

    version 1.1

--- src/application/AirLineController.java
+++ src/application/AirLineController.java
@@ -1,17 +1,130 @@
 package application;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import model.Plane;
+
 public class AirLineController {
 
 	//Constants
-	
-	//Attributes
-		
+
 	//Relations
-		
-	//Constructors
-		
+	@FXML private TextField txtGenerar;
+	@FXML private TextField txtBuscar;
+	@FXML private ListView<String> listi;
+	
+	private ArrayList<Plane> planes;
+	
 	//Getters and Setters
 		
 	//Methods
+	public void initialize() {
+		
+	}
+	
+	public void generarVuelos(ActionEvent event) {
+		
+		if(!txtGenerar.getText().equals("") && Integer.parseInt(txtGenerar.getText()) > 0) {
+			
+			planes = new ArrayList<Plane>();
+			
+			int cantVuelos = Integer.parseInt(txtGenerar.getText());
+			
+			ObservableList<String> vuelos = FXCollections.<String>observableArrayList();
+			
+			for(int i=0; i<cantVuelos; i++) {
+				planes.add(new Plane(i));
+				vuelos.add(planes.get(i).info());
+			}
+			
+			listi.setItems(vuelos);
+			
+		}else {
+			
+			JOptionPane.showMessageDialog(null, "no sea baboso, eliga la cantidad de vuelos a crear (mayor que 0!)", "Mensaje de Advertencia", JOptionPane.WARNING_MESSAGE);
+			
+		}
+		
+	}
+	
+	public void orgFecha(ActionEvent event) {
+		
+		
+		
+	}
+	
+	public void orgHorario(ActionEvent event) {
+		
+		
+		
+	}
+	
+	public void orgAerolinea(ActionEvent event) {
+		
+		
+		
+	}
+	
+	public void orgNumero(ActionEvent event) {
+		
+		
+		
+	}
+	
+	public void orgDestino(ActionEvent event) {
+		
+		
+		
+	}
+	
+	public void orgPuerta(ActionEvent event) {
+		
+		
+		
+	}
+	
+	public void buscFecha(ActionEvent event) {
+		
+		
+		
+	}
+	
+	public void buscHorario(ActionEvent event) {
+		
+		
+		
+	}
+	
+	public void buscAerolinea(ActionEvent event) {
+		
+		
+		
+	}
+	
+	public void buscNumero(ActionEvent event) {
+		
+		
+		
+	}
+	
+	public void buscDestino(ActionEvent event) {
+		
+		
+		
+	}
+	
+	public void buscPuerta(ActionEvent event) {
+		
+		
+		
+	}
 	
 }
--- src/model/Plane.java
+++ src/model/Plane.java
@@ -1,22 +1,166 @@
 package model;
 
 import java.util.Comparator;
+import java.util.Random;
 
 public class Plane implements Comparable, Comparator{
 	
 	//Constants
 	
 	//Attributes
+	private String date;
+	private String time;
+	private String airline;
+	private int id;
+	private String end;
+	private int door;
 	
 	//Relations
 	
 	//Constructors
+	public Plane(int i) {
+		
+		setId(i);
+		
+		generateDate();
+		generateTime();
+		generateAirline();
+		generateEnd();
+		generateDoor();
+		
+	}
 	
 	//Getters and Setters
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getAirline() {
+		return airline;
+	}
+
+	public void setAirline(String airline) {
+		this.airline = airline;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEnd() {
+		return end;
+	}
+
+	public void setEnd(String end) {
+		this.end = end;
+	}
+
+	public int getDoor() {
+		return door;
+	}
+
+	public void setDoor(int door) {
+		this.door = door;
+	}
 	
 	//Methods
+	public void generateDate() {
+		
+		String dt = "";
+		
+		Random r = new Random();
+		int dia = r.nextInt(30)+1;
+		
+		Random f = new Random();
+		int mes = f.nextInt(12)+1;
+
+		Random a = new Random();
+		int año = a.nextInt(79)+1999;
+		
+		dt = dia +"/"+ mes +"/"+ año;
+		
+		setDate(dt);
+		
+	}
+	
+	public void generateTime() {
+		
+		String tim = "";
+		
+		Random r = new Random();
+		int rr = r.nextInt(12)+1;
+		
+		Random f = new Random();
+		int ff = f.nextInt(60);
+		
+		String[] a = {"a.m.", "p.m."};
+		Random aa = new Random();
+		int aaa = aa.nextInt(2);
+		
+		if(ff >= 0 && ff < 10) {
+			tim = rr +":0"+ ff +" "+ a[aaa];
+		}else {
+			tim = rr +":"+ ff +" "+ a[aaa];
+		}
+		
+		setTime(tim);
+		
+	}
+	
+	public void generateAirline() {
+		
+		String[] a = {"Avianca", "American", "LATAM", "Copa", "Delta", "SATENA", "Virgin", "Frontier", "United", "Ethiopian"};
+		Random r = new Random();
+		int d = r.nextInt(10);
+		setAirline(a[d]);
+		
+	}
+	
+	public void generateEnd() {
+		
+		String[] c = {"Kripton", "Earth-chan", "Mordor", "Narnia", "Thanos´s butthole", "you momma", "Equestria", "Mushroom Kingdom", "Hell", "Heaven", "Limbo", "Atlantis", "SS Enterprice", "Galactic Falcon", "Mother Russia blyat!"};
+		Random r = new Random();
+		int d = r.nextInt(15);
+		setEnd(c[d]);
+		
+	}
+	
+	public void generateDoor() {
+		
+		Random r = new Random();
+		int d = r.nextInt(10)+1;
+		setDoor(d);
+		
+	}
+	
+	public String info() {
+		
+		String info = "";
+		
+		info = date +"     "+ time +"      ||      "+ airline +"            "+ id +"                                  "+ end +"      ||      "+ door;
+		
+		return info;
+		
+	}
+	
 	@Override
-	public int compare(Object arg0, Object arg1) {
+	public int compare(Object a, Object b) {
 		return 0;
 	}
 
